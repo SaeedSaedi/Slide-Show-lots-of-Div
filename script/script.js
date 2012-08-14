61de63ffb9e591990036b133334d72ceb7020fb9
SaeedSaedi <Saeed.Saedi.s@gmail.com>
2012-08-15 00:29:50 +0430

    Set background position of each part

--- script/script.js
+++ script/script.js
@@ -1,5 +1,26 @@
 // Javascript
 
 $(function(){
-	
+	var part = $('div.slideshow div.part');
+
+	//set background position of each part
+
+	part.each(function(i){
+		if(i<4){
+			$(this).css({backgroundPosition:(i)*-112.5+"px 0px"})
+		}
+		else
+		if(i<8){
+			$(this).css({backgroundPosition:(i)*-112.5+"px -100px"})
+		}
+		else{
+			$(this).css({backgroundPosition:(i)*-112.5+"px -200px"})
+		}
+	});
+
+
+
+
+
+
 });
